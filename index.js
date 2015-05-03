dea0f2b3f158026fd1ea1f88871af2b600636848
Mathieu Garcia <mgarcia@wiseflat.com>
2015-05-03 22:41:55 +0200

    bug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var xplemail = require("./lib/xpl-clock");
-//var schema_clockconfig = require('/etc/wiseflat/schemas/clock.config.json');
 
 var wt = new xplemail(null, {
 	xplLog: false,
@@ -13,21 +12,10 @@ wt.init(function(error, xpl) {
 		return;
 	}
         
-	//xpl.addBodySchema(schema_clockconfig.id, schema_clockconfig.definitions.body);
-	
-        // Load config file into hash
-        //wt.readConfig();
-        
-
         // Send every minutes an xPL status message 
         setInterval(function(){
-                //wt.sendConfig();
 	       wt.clockTime();
-
         }, 1000);
         
-        xpl.on("xpl:clock.config", function(evt) {
-		if(evt.headerName == 'xpl-cmnd') wt.writeConfig(evt);
-        });
 });
 
